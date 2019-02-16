9ba50feffce92cafd243aa15d8b7b5260191d91d
lamarquenet <nicolaszuain3@hotmail.com>
2019-02-15 22:27:53 -0300

    added port fix and Procfile requiered for naruko starting

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.post("/failure", function(req, res){
   res.redirect("/");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000 , function(){
   console.log("Server started, listening port 3000");
 });
 
